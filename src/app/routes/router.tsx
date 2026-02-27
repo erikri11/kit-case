96ae81966c0dd4a28160114acc180469204cb2c1
Kristian Eriksson <kristian.eriksson@live.no>
2026-02-27 15:38:10 +0100

    Added routes

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,15 +1,15 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-
 import { lazy, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { createRoute } from "./createRoute";
-import { useUserRights } from "../../shared/context/userRights/useUserRights";
 import { CenteredSpinner } from "../../layout/CenteredSpinner";
 import { RoleEnum } from "@shared/types/roleEnum";
+import { useUserRights } from "@shared/context/userRights/useUserRights";
 
-const TasksPage = lazy(() => import("../../pages/TasksPage"));
+
+const TasksPage = lazy(() => import("@pages/TasksPage"));
 // const Orders = lazy(() => import("@pages/Orders/Orders"));
 // const Products = lazy(() => import("@pages/Products/Products"));
-// const Customers = lazy(() => import("@pages/Customers/Customers"));
+const CustomersPage = lazy(() => import("@pages/CustomersPage"));
 
 export function AppRoutes() {
   const { role } = useUserRights();
@@ -20,10 +20,10 @@ export function AppRoutes() {
         {createRoute('/dashboard', <TasksPage />, [role], RoleEnum.USER)}
         {createRoute('/admin/orders', <TasksPage />, [role], RoleEnum.ADMIN)}
         {createRoute('/admin/products', <TasksPage />, [role], RoleEnum.ADMIN)}
-        {createRoute('/admin/customers', <TasksPage />, [role], RoleEnum.ADMIN)}
-        {createRoute('/admin/customers/list', <TasksPage />, [role], RoleEnum.ADMIN)}
-        {createRoute('/admin/customers/create', <TasksPage />, [role], RoleEnum.ADMIN)}
-        {createRoute('/admin/customers/details/:id', <TasksPage />, [role], RoleEnum.ADMIN)}
+        {createRoute('/admin/customers', <CustomersPage />, [role], RoleEnum.ADMIN)}
+        {createRoute('/admin/customers/list', <CustomersPage />, [role], RoleEnum.ADMIN)}
+        {createRoute('/admin/customers/create', <CustomersPage />, [role], RoleEnum.ADMIN)}
+        {createRoute('/admin/customers/details/:id', <CustomersPage />, [role], RoleEnum.ADMIN)}
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </Suspense>
