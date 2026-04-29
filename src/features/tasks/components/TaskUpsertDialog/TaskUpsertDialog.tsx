e0cd334e55f1995fcb811eb836292bdfae87706f
Kristian Eriksson <kristian.eriksson@live.no>
2026-04-29 09:30:25 +0200

    frontend

--- src/features/tasks/components/TaskUpsertDialog/TaskUpsertDialog.tsx
+++ src/features/tasks/components/TaskUpsertDialog/TaskUpsertDialog.tsx
@@ -139,6 +139,7 @@ export function TaskUpsertDialog({
                       variant: "filled"
                     }
                   }}
+                  disablePast
                 />
               </Grid>
             </>
